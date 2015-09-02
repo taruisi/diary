35b83b4140067aac296a6174eb7aa84768c0d2f2
TARUISI Hiroaki <taruishi.hiroak@jp.fujitsu.com>
2015-09-02 12:50:00 +0900

    - テストを２件追加

--- spec/entities_spec.rb
+++ spec/entities_spec.rb
@@ -1,5 +1,13 @@
 require_relative '../lib/entities'
 
-RSpec.describe Entity do
-
+describe Entity do
+  describe "Creation" do
+    let(:entity) { Entity.new('2015/10/10') }
+    it 'Generated from string' do
+      expect(entity.dayspec).to eq "2015/10/10"
+    end
+    it 'arraynized correctly' do
+      expect(entity.to_a.size).not_to eq 0
+    end
+  end
 end
